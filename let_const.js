b8ffe4f73b771f0cdafd959beee5d34647926448
Hunter Powell <hunterp1510@gmail.com>
2022-10-29 20:29:35 -0500

    first commit

---
+++ let_const.js
@@ -0,0 +1,21 @@
+// ES2015 Global Constants
+
+const PI = 3.14;
+
+//Quiz
+
+// What is the difference between var and let?
+
+//let cannot be redeclared while var can
+
+// What is the difference between var and const?
+
+// var can be reassigned and redeclared while const cannot
+
+// What is the difference between let and const?
+
+// let can be reassign while const can not
+
+// What is hoisting?
+
+//'var' variables will be declared before all code is run and 'let' and 'const' will not get hoisted
